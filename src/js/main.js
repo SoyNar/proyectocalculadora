fc83e267549de82ed0eb15ba11753c42402a6ee2
nar mena <narciris.tech@gmail.com>
2024-04-19 07:22:51 -0500

    agregando calculadora sencilla

---
+++ src/js/main.js
@@ -0,0 +1,48 @@
+let resultado;
+
+let menu = ` Menu
+          1. sumar
+          2. restar
+          3. multiplicar
+          4. dividir
+          5. salir
+`;
+
+
+
+
+
+
+
+let calcula = (option, num1, num2) => {
+    console.log(menu);
+    if(option == 1){
+        return num1 + num2;
+    }else if (option == 2){
+        return num1 - num2;
+    }else if(option== 3){
+        return num1* num2;
+    }else if(option == 4){
+        return (num1 / num2 && num2 !== 0);
+    }else if(option == 5){
+        console.log("Saliendo del sistema");
+    }else{
+        console.log("error");
+    }
+}
+let Options = () =>{
+    let option = Number(prompt("Escoja la operacion"));
+    let num1 = parseFloat(prompt("escriba el primer numero"));
+    let num2 = parseFloat(prompt("escriba el segundo numero"));
+}
+
+if(!isNaN(num1) && !isNaN(num2)){
+ resultado = calcula(option, num1, num2);
+ console.log(`resultado: ${resultado}`);
+
+
+}else{
+    console.log(`numeros no validos`);
+}
+
+calcula(option, num1, num2);
